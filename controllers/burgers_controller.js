f7f8a0a281ba2b8bc7da7a67855341f143be0621
Jr <jgrantlyons@gmail.com>
2019-03-23 12:36:54 -0700

    template completed

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// get homepage
+
+// post burger and redirect to homepage
+
+// update burger then redirect to homepage
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,9 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(){},
+    create: function(){},
+    update: function(){},
+}
+
+module.exports = burger;
